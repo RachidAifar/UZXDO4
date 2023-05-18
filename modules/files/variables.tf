371b83942270b3e56a78067dd8bab72a41c9d80b
Rachid Aifar <rachid.aifar24@gmail.com>
2023-05-18 15:01:06 +0200

    Update variables.tf

--- modules/files/variables.tf
+++ modules/files/variables.tf
@@ -1,14 +1,8 @@
-# variable "number_of_files" {
-#   description = "Number of files to create"
-#   type        = number
-#   default     = 3
-# }
 variable "number_of_files" {
   description = "Number of files to create"
-  type        = 3
+  type        = number
+  default     = 3
 }
-
-
 variable "content" {
   description = "Content of the files"
   type        = string
