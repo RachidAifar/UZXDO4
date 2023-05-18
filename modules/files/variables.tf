cfdb06f9fc1fe7d7c41aa163e5ad8455409955d8
RachidAifar <90982692+RachidAifar@users.noreply.github.com>
2023-05-18 14:34:23 +0200

    fix<>:change the varivale

--- modules/files/variables.tf
+++ modules/files/variables.tf
@@ -1,13 +1,8 @@
-# variable "number_of_files" {
-#   description = "Number of files to create"
-#   type        = number
-#   default     = 3
-# }
 variable "number_of_files" {
-  description = "Number of files to create"
-  type        = 3
+  description = "How many files to create"
+  type        = number
+  default     = 3
 }
-
 
 variable "content" {
   description = "Content of the files"
